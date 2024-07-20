f3ae68657cc17d1e4a6676028542926e637526cc
Мехди Арсаханов <mekhdi@MacBook-Air.local>
2024-07-20 08:47:13 +0400

    add new tests

---
+++ tests/UtilsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Hexlet\Phpunit\Tests;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use function Hexlet\Phpunit\Utils\reverseString;
+
+class UtilsTest extends TestCase
+{
+    public function testReverse(): void
+    {
+        $this->assertEquals('', reverseString(''));
+        $this->assertEquals('olleh', reverseString('hello'));
+    }
+}
